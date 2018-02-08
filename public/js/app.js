8ff736d54b753f3b1bc65ad0a75a1f551a115ef3
LizbethFP <lizbeth.fp@gmail.com>
2018-02-08 05:40:26 -0500

    añadiendo CSS

---
+++ public/js/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function ($) {
+  var directionsDisplay = '';
+  var directionsService = new google.maps.DirectionsService();
+  var map = '';
+
+  var initialize = function initialize() {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var coordinates = new google.maps.LatLng(53.401686, -2.165961);
+    var mapOptions = {
+      zoom: 10,
+      center: coordinates
+    };
+    new google.maps.places.Autocomplete(document.querySelector('#start'));
+    new google.maps.places.Autocomplete(document.querySelector('#end'));
+
+    map = new google.maps.Map(document.querySelector('#map-canvas'), mapOptions);
+    directionsDisplay.setMap(map);
+  };
+
+  var searchRoute = function searchRoute() {
+    var inputStart = document.querySelector('#start').value;
+    var inputend = document.querySelector('#end').value;
+    var request = {
+      origin: inputStart,
+      destination: inputend,
+      travelMode: google.maps.TravelMode.DRIVING
+    };
+
+    directionsService.route(request, function (response, status) {
+      if (status === google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      } else {
+        alert('Lo sentimos,no se pudo identificar una ruta entre estas ubicaciones');
+      }
+    });
+  };
+
+  var buttonRoute = document.querySelector('#route');
+  buttonRoute.addEventListener('click', searchRoute);
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+})(jQuery);
